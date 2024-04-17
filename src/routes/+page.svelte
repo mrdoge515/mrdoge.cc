0f38dca5dc69e0e031923a0d0c728469a9e6600d
mrdoge <mrdogerrr@gmail.com>
2024-04-17 20:29:22 +0200

    Make landing page

---
+++ src/routes/+page.svelte
@@ -0,0 +1,3 @@
+<main class="min-h-[100dvh] flex flex-col items-center justify-center bg-zinc-100 dark:bg-zinc-900">
+    <h1 class="text-4xl text-zinc-900 dark:text-zinc-100">Haiii, this is a landing page :3</h1>
+</main>
