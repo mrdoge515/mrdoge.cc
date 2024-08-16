28104d3c716897da8bb98662e4e8d1caffbb8523
mrdoge <mrdogerrr@gmail.com>
2024-08-16 18:42:10 +0200

    feat: add main page

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -1,1 +1,5 @@
-// place files you want to import through the `$lib` alias in this folder.
+export type LinkObject = {
+  url: string;
+  blank: boolean;
+  name: string;
+};
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,56 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script lang="ts">
+  import Title from "$lib/components/Title.svelte";
+  import Link from "$lib/components/Link.svelte";
+  import type { LinkObject } from "$lib";
+
+  let links: LinkObject[] = [
+    { url: "https://github.com/mrdoge515", blank: true, name: "Github" },
+    { url: "/projects", blank: false, name: "Projects" },
+  ];
+
+  let linksStyle: string = "";
+
+  setTimeout(() => {
+    linksStyle = "opacity: 1;";
+  }, 2700);
+</script>
+
+<main>
+  <Title text="mrdoge.cc" typewriterSpeed={300} />
+  <div style={linksStyle}>
+    {#each links as link}
+      <Link url={link.url} blank={link.blank}>{link.name}</Link>
+    {/each}
+  </div>
+</main>
+
+<style>
+  main {
+    min-height: 100vh;
+    min-height: 100dvh;
+
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+
+    background-color: #eff1f5;
+  }
+  div {
+    max-width: 30%;
+    display: flex;
+    flex-direction: row;
+    justify-content: center;
+    align-items: center;
+    flex-wrap: wrap;
+    gap: 2em;
+    opacity: 0;
+    transition: opacity ease-in-out 0.5s;
+  }
+
+  @media (prefers-color-scheme: dark) {
+    main {
+      background-color: #24273a;
+    }
+  }
+</style>
