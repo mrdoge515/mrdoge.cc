b3d8ad16ed4fe646515042cee8cded4f426fbbb8
mrdoge <mrdogerrr@gmail.com>
2024-08-16 17:04:24 +0200

    feat: add Title component
    Includes "typewriter" function that will add letters to displayed text. If user doesn't specify typewriterSpeed attribute it will instantly show on screen.

---
+++ src/lib/components/Title.svelte
@@ -0,0 +1,33 @@
+<script lang="ts">
+  import "@fontsource/ubuntu-mono";
+
+  export let text: string;
+  export let typewriterSpeed: number;
+
+  let displayText: string = "";
+
+  function addLetter(text: string, iteration: number): void {
+    if (iteration >= text.length) {
+      return;
+    }
+
+    setTimeout(() => {
+      displayText += text.charAt(iteration);
+      iteration++;
+      addLetter(text, iteration);
+      console.log("Git");
+    }, typewriterSpeed);
+  }
+
+  addLetter(text, 0);
+</script>
+
+<h1>{displayText}</h1>
+
+<style>
+  h1 {
+    font-family: "Ubuntu Mono", monospace;
+    color: #4c4f69;
+    font-size: 5em;
+  }
+</style>
