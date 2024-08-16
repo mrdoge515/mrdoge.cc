e1fe1a506716842e96831e18a88c96f77116f46a
mrdoge <mrdogerrr@gmail.com>
2024-08-16 17:49:42 +0200

    fix: prevent layout shifting

--- src/lib/components/Title.svelte
+++ src/lib/components/Title.svelte
@@ -4,7 +4,7 @@
   export let text: string;
   export let typewriterSpeed: number;
 
-  let displayText: string = "";
+  let displayText: string = text.charAt(0);
 
   function addLetter(text: string, iteration: number): void {
     if (iteration >= text.length) {
@@ -15,11 +15,10 @@
       displayText += text.charAt(iteration);
       iteration++;
       addLetter(text, iteration);
-      console.log("Git");
     }, typewriterSpeed);
   }
 
-  addLetter(text, 0);
+  addLetter(text, 1);
 </script>
 
 <h1>{displayText}</h1>
